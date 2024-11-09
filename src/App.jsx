35706a494ea2d79e5e6afc0d9d1e16f3006bef99
aditya123473892 <adityathakur6199@gmail.com>
2024-11-09 10:25:49 +0530

    fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
-import './App.css'
-import WebsiteGrader from './components/WebsiteGrader'
-import Main from './components/Main'
-import Lighthouse from './components/lighthouse'
+import "./App.css";
+import WebsiteGrader from "./components/WebsiteGrader";
+import Main from "./components/Main";
+import Lighthouse from "./components/lighthouse";
+import PageSpeedCards from "./components/Appspeedcard";
+import PageSpeedInsights from "./Pagespeed";
 
 function App() {
-
   return (
     <>
-      <WebsiteGrader/>
-      <Main/>
+      <WebsiteGrader />
+      <Main />
+      <PageSpeedCards></PageSpeedCards>
+      <PageSpeedInsights></PageSpeedInsights>
+
       {/* <Lighthouse/> */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
